2a5e2cb9222eee286f1e206d3c29b992d508f39b
Rohith <153813697+varma6600666@users.noreply.github.com>
2024-12-20 11:05:48 +0530

    Create Exp1.java

---
+++ Exp1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 5.0f, b = 2.0f, c = -5.0f, x = 7.0f, y;
+        y = (a * x * x) + (b * x) + c;
+        System.out.printf("For x = %.2f, the result is y = %.2f%n", x, y);
+    }
+}
